713df328a50c949d4c230791ea6068ac3cb0f195
Alexander Curlett <RC0024757@student.riversidecollege.ac.uk>
2017-09-27 10:03:59 +0100

    Add project files.

---
+++ Question And Answer/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Question_And_Answer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome " + textBox1.Text + " The Question Is: 17-2X8=1,Yes or No?");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Correct " + textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Incorrect " + textBox1.Text);
+        }
+    }
+}
